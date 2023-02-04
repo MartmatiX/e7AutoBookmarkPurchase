c6adb8f572b83aa7dd124e899c3ece5720045395
MartmatiX <martin.malir@gmail.com>
2023-02-04 16:44:41 +0100

    SemiFinished Project

---
+++ main.py
@@ -0,0 +1,84 @@
+import time
+import pyautogui
+import keyboard
+
+import sys
+
+
+def refresh():
+    # Click Refresh button
+    print("Refreshing shop...")
+    refreshCoordinates = None
+    while refreshCoordinates is None:
+        refreshCoordinates = pyautogui.locateCenterOnScreen("refresh_button.PNG")
+    pyautogui.click(refreshCoordinates)
+    time.sleep(0.2)
+
+
+def confirm():
+    # Click Confirm button button
+    print("Confirming refresh...")
+    confirmCoordinates = None
+    while confirmCoordinates is None:
+        confirmCoordinates = pyautogui.locateCenterOnScreen("confirm_button.png")
+    pyautogui.click(confirmCoordinates)
+    time.sleep(1)
+
+
+def findCovenantBookmarks():
+    # Find Covenant Bookmarks
+    print("Searching for Covenant Bookmarks...")
+    covenantCoordinates = None
+    covenantCoordinates = pyautogui.locateCenterOnScreen("covenant_bookmarks.png", confidence=0.8)
+    if covenantCoordinates is None:
+        print("Bookmarks not found...")
+    else:
+        pyautogui.moveTo(covenantCoordinates)
+        pyautogui.click(pyautogui.position().x + 800, pyautogui.position().y)
+        time.sleep(0.2)
+        pyautogui.click(pyautogui.locateCenterOnScreen("purchase_covenant_bookmark.png"))
+        time.sleep(1)
+
+
+def findMysticMedals():
+    # Find Mystic Medals
+    print("Searching for Mystic Medals...")
+    mysticCoordinates = None
+    mysticCoordinates = pyautogui.locateCenterOnScreen("mystic_medals.png", confidence=0.8)
+    if mysticCoordinates is None:
+        print("Medals not found...")
+    else:
+        pyautogui.moveTo(mysticCoordinates)
+        pyautogui.click(pyautogui.position().x + 800, pyautogui.position().y)
+        time.sleep(0.2)
+        pyautogui.click(pyautogui.locateCenterOnScreen("purchase_mystic_medals.png"))
+        time.sleep(1)
+
+
+def scrollShop():
+    print("Scrolling...")
+    pyautogui.scroll(-3)
+    time.sleep(1)
+
+
+def kb():
+    while True:
+        if keyboard.is_pressed("r"):
+            refresh()
+            confirm()
+            findCovenantBookmarks()
+            findMysticMedals()
+            scrollShop()
+            findCovenantBookmarks()
+            findMysticMedals()
+            print("Cycle finished...")
+        if keyboard.is_pressed("e"):
+            print("Exiting...")
+            sys.exit()
+
+
+if __name__ == "__main__":
+    print("Controls:\n"
+          "R for refresh\n"
+          "E for exitr")
+    kb()
